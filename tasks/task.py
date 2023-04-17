a1422c2a4e91ea9f522db7941aca90e857b582a8
Gruzin <gruzd.roman@inbox.ru>
2023-04-17 19:20:33 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -1,2 +1,24 @@
 class Counter:
-    pass
+    current = 0
+    start = 0
+    stop = 0
+    def __init__(self, start=0, stop=0, current=0):
+        self.start = start
+        self.stop = stop
+        self.current = start
+
+    def get(self):
+        print(self.current)
+        return
+
+    def increment(self):
+        self.current = self.current
+        if self.current < self.stop:
+            self.current += 1
+        elif self.start == 0 and self.stop == 0:
+            self.current += 1
+        elif self.start > 0 and self.stop == 0:
+            self.current += 1
+        else:
+            print( 'Maximal value is reached.' )
+        return
